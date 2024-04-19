26af3812f33b4b9d2cbfd587df412d8bded35a01
Krigupt <123670605+Krigupt@users.noreply.github.com>
2024-04-18 21:09:37 -0700

    a

--- sketch.js
+++ sketch.js
@@ -2,12 +2,20 @@
 // let poseNet;
 // let pose;
 // let skeleton;
+<<<<<<< Updated upstream
 
 // let brain;
 
 // let state = 'waiting';
 // let targetLabel; // Fixed typo here
 
+=======
+// let brain;
+// let state = 'waiting';
+// let targetLabel; // Fixed typo here
+
+
+>>>>>>> Stashed changes
 // function keyPressed() {
 //     if (key == 's') {
 //         brain.saveData();
@@ -27,9 +35,15 @@
 
 // function setup() {
 //     createCanvas(1000, 800);
+<<<<<<< Updated upstream
 //     video = createVideo(['baddi_mine.mp4']);
 //     video.play();
 //     //video.hide();
+=======
+//     video = createVideo(['baddi.mp4']);
+//     video.hide();
+//     video.play();
+>>>>>>> Stashed changes
 //     poseNet = ml5.poseNet(video, modelLoaded);
 //     poseNet.on('pose', gotPoses);
 
@@ -45,6 +59,11 @@
 
 
 
+<<<<<<< Updated upstream
+=======
+
+
+>>>>>>> Stashed changes
 // function gotPoses(poses) {
 //     if (poses.length > 0) {
 //         pose = poses[0].pose;
@@ -73,7 +92,10 @@
 // function draw() {
 //    translate(video.width, 0);
 //     scale(-1, 1);
+<<<<<<< Updated upstream
 //     background(220);
+=======
+>>>>>>> Stashed changes
 //     image(video, 0, 0, video.width, video.height);
 
 //     if (pose) {
@@ -96,7 +118,13 @@
 
 
 
+<<<<<<< Updated upstream
 //CODE WRITTEN BY GPT
+=======
+
+
+
+>>>>>>> Stashed changes
 
 let video;
 let poseNet;
@@ -125,13 +153,18 @@ function keyPressed() {
                 console.log('not collecting');
                 state = 'waiting';
             }, 2000);
-        }, 2000);
+        }, 1000);
     }
 }
 
 function setup() {
+<<<<<<< Updated upstream
     createCanvas(1000, 800);
     video = createVideo(['baddi_mine.mp4']);
+=======
+    createCanvas(1300, 900);
+    video = createVideo(['train_vid.mp4']);
+>>>>>>> Stashed changes
     // video.play(); // Removed from setup
     //video.hide();
     poseNet = ml5.poseNet(video, modelLoaded);
@@ -190,5 +223,9 @@ function draw() {
             ellipse(x, y, 16, 16);
         }
     }
+<<<<<<< Updated upstream
 }
 
+=======
+}
+>>>>>>> Stashed changes
--- sketch_model_training.js
+++ sketch_model_training.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 let brain;
 
 function setup() {
@@ -26,4 +27,35 @@ function finished() {
   console.log('model trained');
   brain.save();
   console.log("hi")
+=======
+
+let brain;
+
+function setup() {
+  console.log("hi1")
+  createCanvas(640, 480);
+  let options = {
+    inputs: 34,
+    outputs: 4,
+    task: 'classification',
+    debug: true
+    
+  }
+  brain = ml5.neuralNetwork(options);
+  brain.loadData('json1.json', dataReady);
+  
+}
+
+function dataReady() {
+  console.log("hi")
+  brain.normalizeData();
+  brain.train({epochs: 50}, finished); 
+  console.log("hi")
+}
+
+function finished() {
+  console.log('model trained');
+  brain.save();
+  console.log("hi")
+>>>>>>> Stashed changes
 }
